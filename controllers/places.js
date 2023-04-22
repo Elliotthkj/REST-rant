abc2e17e81234890c342ab71e885bf1a8765d729
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-22 04:17:13 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,39 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const places = require("../models/places.js");
 
 //new route
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
-
 // index route
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "JINYA Ramen Bar",
-      city: "Las Vegas",
-      state: "NV",
-      cuisines: "Japanese Ramen",
-      pic: "/images/jrb_ramen_tonkotsu-black.jpg",
-    },
-    {
-      name: "Express Hibachi",
-      city: "Las Vegas",
-      state: "NV",
-      cuisines: "Chinese",
-      pic: "/images/express_hibachi.jpg",
-    },
-  ];
-
   res.render("places/index", { places });
 });
 
 router.post("/", (req, res) => {
   console.log(req.body);
-  res.send("POST /places");
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
-
 
 // Export the router
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+      name: "JINYA Ramen Bar",
+      city: "Las Vegas",
+      state: "NV",
+      cuisines: "Japanese Ramen",
+      pic: "/images/jrb_ramen_tonkotsu-black.jpg",
+    },
+    {
+      name: "Express Hibachi",
+      city: "Las Vegas",
+      state: "NV",
+      cuisines: "Chinese",
+      pic: "/images/express_hibachi.jpg",
+    },
+  ];
