5cf38f8bcde643b71b6ca9076ec0314b81430d0b
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-22 12:42:19 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 const places = require("../models/places.js");
 
+// show route
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id] });
+  }
+});
+
 //new route
 router.get("/new", (req, res) => {
   res.render("places/new");
