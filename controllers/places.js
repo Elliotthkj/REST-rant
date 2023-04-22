2df090474e48b1f34593d3479ff651472691a7a5
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-22 14:02:17 -0700

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 const places = require("../models/places.js");
 
+// edit route
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id] });
+  }
+});
+
 // show route
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
@@ -14,7 +26,7 @@ router.get("/:id", (req, res) => {
   }
 });
 
-//new route
+// new route
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
