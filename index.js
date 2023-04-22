f4436721845cf6ff97aa2ca1f7960d310c419c73
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-22 13:32:33 -0700

    delete functionality implemented

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-// Get the environment variables when using .env
+// Modules and Globals
 require("dotenv").config();
-// Require needed modules.
 const express = require("express");
+const methodOverride = require("method-override");
+
 // Initialize the app object.
 const app = express();
 
 // Express Settings
-app.set("views", __dirname + "/views");
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 // places.js module
 const placesRouter = require("./controllers/places");
@@ -22,6 +25,7 @@ app.get("/", function (req, res) {
   // (your web browser most likely!)
   res.render("home");
 });
+
 
 //404 error handler
 app.get("*", (req, res) => {
