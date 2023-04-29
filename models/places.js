4762fd4220bb15c14d395a6f1591a6d121d7b9b7
Elliott Johnson <elliotthkj1991@gmail.com>
2023-04-29 09:51:52 -0700

    implemented mongoose and updated routes

--- models/places.js
+++ models/places.js
@@ -1,16 +1,29 @@
-module.exports = [
-    {
-      name: "JINYA Ramen Bar", // index 0
-      city: "Las Vegas",
-      state: "NV",
-      cuisines: "Japanese Ramen",
-      pic: "/images/jrb_ramen_tonkotsu-black.jpg",
-    },
-    {
-      name: "Express Hibachi", // index 1
-      city: "Las Vegas",
-      state: "NV",
-      cuisines: "Chinese",
-      pic: "/images/express_hibachi.jpg",
-    },
-  ];
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: "Anytown" },
+  state: { type: String, default: "USA" },
+  founded: Number,
+});
+
+module.exports = mongoose.model("Place", placeSchema);
+
+// module.exports = [
+//     {
+//       name: "JINYA Ramen Bar", // index 0
+//       city: "Las Vegas",
+//       state: "NV",
+//       cuisines: "Japanese Ramen",
+//       pic: "/images/jrb_ramen_tonkotsu-black.jpg",
+//     },
+//     {
+//       name: "Express Hibachi", // index 1
+//       city: "Las Vegas",
+//       state: "NV",
+//       cuisines: "Chinese",
+//       pic: "/images/express_hibachi.jpg",
+//     },
+//   ];
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,22 +7,28 @@ function new_form() {
       <main>
         <h1>Add a New Place</h1>
         <form method="POST" action="/places">
-          <div className="form-group">
-            <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+          <div className="row">
+            <div className="form-group col-sm-6">
+              <label htmlFor="name">Place Name</label>
+              <input className="form-control" id="name" name="name" required />
+            </div>
+            <div className="form-group col-sm-6">
+              <label htmlFor="pic">Place Picture</label>
+              <input className="form-control" id="pic" name="pic" />
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" />
+
+          <div className="row">
+            <div className="form-group col-sm-6">
+              <label htmlFor="city">City</label>
+              <input className="form-control" id="city" name="city" />
+            </div>
+            <div className="form-group col-sm-6">
+              <label htmlFor="state">State</label>
+              <input className="form-control" id="state" name="state" />
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="state">State</label>
-            <input className="form-control" id="state" name="state" />
-          </div>
+
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
             <input
@@ -32,6 +38,11 @@ function new_form() {
               required
             />
           </div>
+          <div className="form-group">
+            <label for="founded">Founded Year</label>
+            <input className="form-control" id="founded" name="founded" />
+          </div>
+
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
       </main>
